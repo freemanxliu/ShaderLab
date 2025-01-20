a925d79008bb1bbf1411b8ddc7ac3a44ac7ad635
freemanxliu <63280652+freemanxliu@users.noreply.github.com>
2025-01-21 00:32:32 +0800

    shader尝试

--- Tools/Source.hlsl
+++ Tools/Source.hlsl
@@ -1,22 +1,43 @@
 int SampleCount;
 #define SAMPLE_COUNT 10
 
+Texture2D BaseTexture;
+SamplerState BaseSampler
+{
+    Filter = MIN_MAG_MIP_LINEAR ;
+    AddressU = Wrap;
+    AddressV = Wrap;
+};
+
 float4 MainPS(float4 Position : SV_POSITION) : SV_TARGET
 {
-	int result = 0.1;
+	float result = 0.1;
+	float4 color = 0.0;
 
 	// FOR 循环 -- 常量作为循环条件
-	for(int i = 0; i < SAMPLE_COUNT; i ++)
-	{
-		result += Position.x;
-	}
-
-	// FOR 循环 -- uniform变量作为循环条件
-	// for(int i = 0; i < SampleCount; i ++)
+	// [unroll]
+	// for(int i = 0; i < SAMPLE_COUNT; i ++)
 	// {
-	// 	result += Position.x;
+	// 	float2 UV = Position.xy / (i + 1.0);
+	// 	color += BaseTexture.SampleLevel(BaseSampler, UV, 0);
 	// }
 
+	// FOR 循环 -- uniform变量作为循环条件
+	
+	for(int i = 0; i < SampleCount; i ++)
+	{
+		float2 UV = Position.xy / (i + 1.0);
+		color += BaseTexture.Sample(BaseSampler, UV);
+	}
 
-	return float4(result, 0.0, 0.0, 0.0);
+	// float Random = Position.y;
+
+	// [flatten]
+	// if(Position.x < Random)
+	// {
+	// 	result += 0.1;
+	// }
+
+	// return float4(result, 0.0, 0.0, 0.0);
+	return color;
 }
